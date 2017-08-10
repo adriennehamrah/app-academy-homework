be1e41c47d4f9052eb3a8f086e54682cb1fab8cc
adriennelin <lin.adrienne@gmail.com>
2017-08-09 21:38:41 -0700

    Update mancala

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -4,46 +4,54 @@ class Board
   def initialize(name1, name2)
     @name1 = name1
     @name2 = name2
-    @cups = Array.new(14){Array.new([])}
+    @cups = Array.new(14) { Array.new }
     place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
     @cups.each_index do |i|
-      cups[i] += [:stone, :stone, :stone, :stone] unless i == 6 || i ==13
+      next if i == 6 || i == 13
+      @cups[i] += [:stone, :stone, :stone, :stone]
     end
   end
 
   def valid_move?(start_pos)
-    raise 'Invalid starting cup' unless [0..13].include?(start_pos)
+    raise 'Invalid starting cup' if start_pos < 0 || start_pos > 12
+    raise 'Invalid starting cup' if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-    stones = cups[start_pos]
-    cups[start_pos] = []
-    curr_cup_idx = start_pos + 1
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+
     until stones.empty?
-      curr_cup_idx = 0 if curr_cup_idx == 14
-      if current_player_name == @name1
-        @cups[curr_cup_idx] << stones.pop unless curr_cup_idx == 13
-      elsif current_player_name == @name2
-        @cups[curr_cup_idx] << stones.pop unless curr_cup_idx == 6
+      cup_idx += 1
+      cup_idx = 0 if cup_idx == 14
+      case cup_idx
+      when 6
+        @cups[cup_idx] << stones.pop if current_player_name == @name1
+      when 13
+        @cups[cup_idx] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
       end
-      curr_cup_idx += 1
+
     end
+
     render
-    next_turn(curr_cup_idx - 1)
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-
     if ending_cup_idx == 6 || ending_cup_idx == 13
       :prompt
-    elsif @cups[ending_cup_idx].count == 1
+    elsif @cups[ending_cup_idx].size == 1
       :switch
-    elsif @cups[ending_cup_idx].count > 0
+    elsif @cups[ending_cup_idx].size > 0
       ending_cup_idx
     end
   end
@@ -57,15 +65,20 @@ class Board
   end
 
   def one_side_empty?
-    @cups[0..5].all?(&:empty?) || @cups[0..5].all?(&:empty?)
+    @cups[0..5].all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
-    if @cups[6].count == @cups[13].count
-      :draw
-    elsif @cups[6].count < @cups[13].count
+    player1_score = @cups[6].count
+    player2_score = @cups[13].count
+
+    case player1_score <=> player2_score
+    when -1
       @name2
-    else
+    when 0
+      :draw
+    when 1
       @name1
     end
   end
