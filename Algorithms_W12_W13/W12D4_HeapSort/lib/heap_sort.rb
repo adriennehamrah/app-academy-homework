4b139b01237f80fd43e51e2ff4d713b4a3f0d24e
Adrienne Lin <lin.adrienne@gmail.com>
2017-10-19 14:06:06 -0700

    Update heapsort, add k largest element, passes specs

--- Algorithms_W12_W13/W12D4_HeapSort/lib/heap_sort.rb
+++ Algorithms_W12_W13/W12D4_HeapSort/lib/heap_sort.rb
@@ -2,14 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    prc ||= Proc.new { |a, b| a <=> b }
-    heap = BinaryMinHeap.new(&prc)
-    self.each do |el|
-      heap.push(el)
+    # minHeap - need to reverse at end
+    2.upto(count).each do |heap_sz|
+      BinaryMinHeap.heapify_up(self, heap_sz - 1, heap_sz)
     end
 
-    BinaryMinHeap.heapify_down(heap.store, 0, heap.count, &prc)
+    count.downto(2).each do |heap_sz|
+      self[heap_sz - 1], self[0] = self[0], self[heap_sz - 1]
+      BinaryMinHeap.heapify_down(self, 0, heap_sz - 1)
+    end
 
-    heap
+    self.reverse!
   end
 end
--- Algorithms_W12_W13/W12D4_HeapSort/lib/k_largest_elements.rb
+++ Algorithms_W12_W13/W12D4_HeapSort/lib/k_largest_elements.rb
@@ -1,5 +1,18 @@
 require_relative 'heap'
 
+# time: O(nlogk)
 def k_largest_elements(array, k)
+  res = BinaryMinHeap.new
 
+  # use heap that is size k
+  k.times do
+    res.push(array.pop)
+  end
+
+  until array.empty?
+    res.push(array.pop)
+    res.extract # maintain a heap of size k
+  end
+
+  res.store
 end
